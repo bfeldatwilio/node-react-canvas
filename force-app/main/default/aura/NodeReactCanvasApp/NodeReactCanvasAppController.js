9c208b9c71528e68c3f51a5a9716e200ab6d5446
bfelda <bfelda@gmail.com>
2022-03-07 16:40:47 -0600

    removed extra code from the Aura component

--- force-app/main/default/aura/NodeReactCanvasApp/NodeReactCanvasAppController.js
+++ force-app/main/default/aura/NodeReactCanvasApp/NodeReactCanvasAppController.js
@@ -1,27 +1,19 @@
 ({
-    doInit : function(component, evt, helper) {
-        var recordId = component.get("v.recordId");
-        // var product = component.get("c.FY_16_Primary_Product__c");
-        // console.log('******* record Id ********' + recordId);
-        // console.log('******* record type ********' + recordType);
-        // console.log('******* Product ********' + product);
-        component.set("v.canvasParameters", JSON.stringify({
-            recordId: recordId
-        }));
-    },
+	doInit: function (component, evt, helper) {
+		var recordId = component.get("v.recordId");
+		component.set(
+			"v.canvasParameters",
+			JSON.stringify({
+				recordId: recordId,
+			})
+		);
+	},
 
-    onCanvasLoad : function(component, evt, helper) {
-        console.log("^^^^^^^^^^^^^^^^^ Canvas Loaded React ^^^^^^^^^^^^^^^^^");
-        window.addEventListener('message', function(event) {
-            var data = JSON.parse(event.data);
-            if(data.targetModule === 'Canvas' && data.body?.event?.name) {
-                console.log('*****Received event from Canvas App **********');
-                
-            }
-        });
-    },
+	onCanvasLoad: function (component, evt, helper) {
+		console.log("^^^^^^^^^^^^^^^^^ Canvas Loaded React ^^^^^^^^^^^^^^^^^");
+	},
 
-    onCanvasSubscribed : function(component, event, helper) {
-        console.log('^^^^^^^^^^^^^^^^^^^^Canvas Subscribed^^^^^^^^^^^^^^^^^');
-    }
+	onCanvasSubscribed: function (component, event, helper) {
+		console.log("^^^^^^^^^^^^^^^^^^^^Canvas Subscribed^^^^^^^^^^^^^^^^^");
+	},
 });
